145811331c4f9e857362b69539abf049e7128d56
ltpaddy <ltpaddy5@gmail.com>
2018-08-23 14:11:54 -0500

    Add project files.

---
+++ ClearentCodeChallenge/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClearentCodeChallenge
+{
+    public class Card : IInterestCalc
+    {
+        public virtual float InterestRate {  get { return 0; } }
+
+        private decimal balance;
+
+        public Card(decimal balance)
+        {
+            this.balance = balance;
+        }
+
+        public decimal CalculateInterest()
+        {
+            return ((decimal)InterestRate) * balance;
+        }
+    }
+}
+
+
---
+++ ClearentCodeChallenge/IInterestCalc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearentCodeChallenge
+{
+    public interface IInterestCalc
+    {
+        decimal CalculateInterest();
+    }
+}
